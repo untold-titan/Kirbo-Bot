3787202d3f7e422b47c01bdd31e7861998baba96
Mitch <untoldtitan3k@gmail.com>
2022-06-13 11:22:33 -0600

    Added Confirmation

--- Commands/MTG.py
+++ Commands/MTG.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from requests import request
 import requests
@@ -17,66 +18,76 @@ class MTGCog(commands.Cog):
     # DO NOT TOUCH THIS HOLY FUCK THIS IS PRECARIUOUS AS FUCK
     @commands.command(name="won")
     async def won(self,ctx,member:MemberConverter):
-        winner = Helper.getUserData(ctx.author.id)
-        loser = Helper.getUserData(member.id)
-        if winner != None and loser != None:
-            #in the case that both users already have played against each other
-            ids = winner["playedVS"].split(",")
-            wins = winner["wins"].split(",")
-            if(winner["playedVS"] != ""):
-                for i in range(len(ids)):
-                    if(str(member.id) == ids[i]):
-                        win = int(wins[i])
-                        win += 1
-                        wins[i] = str(win)
-                        winner["wins"] = ""
-                        for win in wins:
-                            winner["wins"] += win + ","
-                        winner["wins"] = winner["wins"].rstrip(winner["wins"][-1])
-                    elif(member.id != ids[i] and i == len(ids) - 1):
-                        winner["playedVS"] += "," + str(member.id)
-                        winner["wins"] += ",1"
-                        winner["losses"] += ",0"
-            ids = loser["playedVS"].split(",")
-            losses = loser["losses"].split(",")
-            if(loser["playedVS"] != ""):
-                for i in range(len(ids)):
-                    if(str(ctx.author.id) == ids[i]):
-                        lose = int(losses[i])
-                        lose += 1
-                        losses[i] = str(lose)
-                        loser["losses"] = ""
-                        for lose in losses:
-                            loser["losses"] += lose + ","
-                        loser["losses"] = loser["losses"].rstrip(loser["losses"][-1])
-                    elif(member.id != ids[i] and i == len(ids) - 1):
-                        loser["playedVS"] += "," + str(ctx.author.id)
-                        loser["wins"] += ",0"
-                        loser["losses"] += ",1"
-            #For when the winner has no values set
-            if winner["playedVS"] == "":
-                winner["playedVS"] = str(member.id)
-                winner["wins"] = "1"
-                winner["losses"] = "0"
-                res = requests.put(USER_URL + str(ctx.author.id),json=winner)
-            #For when the loser has no values set
-            if loser["playedVS"] == "":
-                loser["playedVS"] = str(ctx.author.id)
-                loser["wins"] = "0"
-                loser["losses"] = "1"
-                res = requests.put(USER_URL + str(member.id),json=loser)
-            res = requests.put(USER_URL + str(ctx.author.id),json=winner)
-            res = requests.put(USER_URL + str(member.id),json=loser)
-        elif(winner == None):
-            await ctx.send("You need to create a Kirbo account! Use `,bal` or `,daily` to start!")
-        elif(loser == None):
-            await ctx.send(f"`{member.name}` needs to create a Kirbo account! Use `,bal` or `,daily` to start!")
+        await ctx.send(f"{member} type `y` to confirm that you LOST against {ctx.author.name}")
+        def check(m: discord.Message):
+            return m.author.id == member.id and m.channel.id == ctx.channel.id and m.content == "y" or "Y" 
+        try:
+            msg = await self.bot.wait_for(event = 'message', check = check, timeout = 60.0)
+        except asyncio.TimeoutError:
+            await ctx.send(f"Automatically declined win.")
+            return
+        else:
+                winner = Helper.getUserData(ctx.author.id)
+                loser = Helper.getUserData(member.id)
+                if winner != None and loser != None:
+                    #in the case that both users already have played against each other
+                    ids = winner["playedVS"].split(",")
+                    wins = winner["wins"].split(",")
+                    if(winner["playedVS"] != ""):
+                        for i in range(len(ids)):
+                            if(str(member.id) == ids[i]):
+                                win = int(wins[i])
+                                win += 1
+                                wins[i] = str(win)
+                                winner["wins"] = ""
+                                for win in wins:
+                                    winner["wins"] += win + ","
+                                winner["wins"] = winner["wins"].rstrip(winner["wins"][-1])
+                            elif(member.id != ids[i] and i == len(ids) - 1):
+                                winner["playedVS"] += "," + str(member.id)
+                                winner["wins"] += ",1"
+                                winner["losses"] += ",0"
+                    ids = loser["playedVS"].split(",")
+                    losses = loser["losses"].split(",")
+                    if(loser["playedVS"] != ""):
+                        for i in range(len(ids)):
+                            if(str(ctx.author.id) == ids[i]):
+                                lose = int(losses[i])
+                                lose += 1
+                                losses[i] = str(lose)
+                                loser["losses"] = ""
+                                for lose in losses:
+                                    loser["losses"] += lose + ","
+                                loser["losses"] = loser["losses"].rstrip(loser["losses"][-1])
+                            elif(member.id != ids[i] and i == len(ids) - 1):
+                                loser["playedVS"] += "," + str(ctx.author.id)
+                                loser["wins"] += ",0"
+                                loser["losses"] += ",1"
+                    #For when the winner has no values set
+                    if winner["playedVS"] == "":
+                        winner["playedVS"] = str(member.id)
+                        winner["wins"] = "1"
+                        winner["losses"] = "0"
+                        res = requests.put(USER_URL + str(ctx.author.id),json=winner)
+                    #For when the loser has no values set
+                    if loser["playedVS"] == "":
+                        loser["playedVS"] = str(ctx.author.id)
+                        loser["wins"] = "0"
+                        loser["losses"] = "1"
+                        res = requests.put(USER_URL + str(member.id),json=loser)
+                    res = requests.put(USER_URL + str(ctx.author.id),json=winner)
+                    res = requests.put(USER_URL + str(member.id),json=loser)
+                    ctx.send("Updated Stats!")
+                elif(winner == None):
+                    await ctx.send("You need to create a Kirbo account! Use `,bal` or `,daily` to start!")
+                elif(loser == None):
+                    await ctx.send(f"`{member.name}` needs to create a Kirbo account! Use `,bal` or `,daily` to start!")
+        return
     
     @commands.command(name="stats")
     async def stats(self,ctx):
         user = Helper.getUserData(ctx.author.id)
         players = user["playedVS"].split(",")
-        print(players)
         wins = user["wins"].split(",")
         losses = user["losses"].split(",")
         embed = discord.Embed(title=f"{ctx.author.name}'s MTG stats",color=Helper.PINK)
