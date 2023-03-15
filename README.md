0a476420df701b73d4aff5f059aaa18e2f3f4297
Titan <untoldtitan3k@gmail.com>
2023-03-15 09:33:37 -0600

    Added 2 commands, rewriting in JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Kirbo-Bot
-The discord bot built for the Army Gang Gaming Community
+# Kirbo Bot v2
+Written In JS
+
+## Project Goals
+- To create a simple discord bot to complete tasks around the Army Gang Discord Server
+- To utilise slash commands to make interactions with the bot way easier, and less confusing.
---
+++ commands/add-lfg-role.js
@@ -0,0 +1,17 @@
+const { ActionRowBuilder, StringSelectMenuBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
+const {roles} = require("../roles.json");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('add_lfg_role')
+		.setDescription('Adds an LFG Role to the user'),
+	async execute(interaction) {
+        const roleSelect = new ActionRowBuilder()
+        .addComponents(
+            new StringSelectMenuBuilder().setCustomId("role_select").setPlaceholder("Pick a role!")
+            .addOptions(roles)
+        )
+		await interaction.reply({content:"Please pick a role!", components:[roleSelect]});
+	},
+};
